6cc4a2bce26f7c2dd0de52056dedcc05cac353cc
Anthony Leif <tonyleif@hotmail.com>
2017-11-09 11:03:02 -0600

    Page 541

--- WorkingWithRazor/Global.asax.cs
+++ WorkingWithRazor/Global.asax.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using WorkingWithRazor.Infrastructure;
 
 namespace WorkingWithRazor
 {
@@ -13,6 +14,9 @@ namespace WorkingWithRazor
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new CustomLocationViewEngine());
         }
     }
 }
